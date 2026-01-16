78cf9e57c46705c734d5df726ddfca7d4df14fdb
Naz22135 <mdnazmulhossan360@gmail.com>
2026-01-17 00:18:24 +0600

    login page done

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 // loginn button functionality
 document.getElementById('loginButton').addEventListener('click', function (e) {
     e.preventDefault()
-    console.log('login click')
+
+    const mobileNumber = 17429784580
+    const pinNumber = 2020
+
+    const mobileNumberValue = document.getElementById('mobile-number').value
+    const mobileNumberValueConvert = parseInt(mobileNumberValue)
+
+    const pinNumberValue = document.getElementById('pin-number').value
+    const pinNumbervalueConvert = parseInt(pinNumberValue)
+
+    if (mobileNumberValueConvert === mobileNumber && pinNumbervalueConvert === pinNumber) {
+        window.location.href = './home.html'
+    }
+    else {
+        alert('Invalid credentials')
+    }
+
+
+
+
 })
